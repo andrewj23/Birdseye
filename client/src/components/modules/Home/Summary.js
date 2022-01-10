3c3f855dd18ebac93996fac42276878262c7ee6e
Nicholas Gustafson <nickgust@mit.edu>
2022-01-10 18:13:24 -0500

    Summary CSS

--- client/src/components/modules/Home/Summary.js
+++ client/src/components/modules/Home/Summary.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import "./Summary.css";
+
 const Summary = (props) => {
   return (
-    <p>Summary</p>
-  );
+
+    <div className="Summary-container">
+      <span className="Summary-PV">$420,069</span>
+      <span className="Summary-percentage">-1.83%</span>
+      <div className="Summary-principal">Principal: $500,000</div>
+      <div className="Summary-change">Net Change: -$79,931</div>
+    </div>
+  
+    );
 };
 
 export default Summary;
