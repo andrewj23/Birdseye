b673828e815207696f668bbc92e29485175f1e63
andrewj23 <apj785@gmail.com>
2022-01-11 16:13:56 -0500

    minor

--- client/src/components/modules/Home/Feed.js
+++ client/src/components/modules/Home/Feed.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
 import CoinCard from "./CoinCard";
+// const CoinGecko = require('coingecko-api');
+
+//2. Initiate the CoinGecko API Client
+// const CoinGeckoClient = new CoinGecko();
+
+//3. Make calls
+// var func = async() => {
+//   let data = await CoinGeckoClient.ping();
+// };
 
 const coins = [
   {
