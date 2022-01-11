9a801eb134f9dd0fd018b8331e4e6c4938172036
Nicholas Gustafson <nickgust@mit.edu>
2022-01-10 20:02:51 -0500

    Began building Wallets page

---
+++ client/src/components/modules/Wallets/SearchBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./SearchBar.css";
+
+const SearchBar = (props) => {
+  return (
+    <input type="text" placeholder="Search"></input>
+  );
+};
+
+export default SearchBar;
---
+++ client/src/components/pages/Wallets.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import "../../utilities.css";
+import "./Wallets.css";
+import SearchBar from "../modules/Wallets/SearchBar";
+import Feed from "../modules/Wallets/Feed";
+import WalletCard from "../modules/Wallets/WalletCard";
+
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+
+const Wallets = ({ userId, handleLogin, handleLogout }) => {
+    return (
+        <div>
+            <SearchBar />
+            <p>hi</p>
+        </div>
+    );
+};
+
+export default Wallets;
