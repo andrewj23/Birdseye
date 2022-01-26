ee723824fa6b948801b03850154f8881624a0145
Nicholas Gustafson <nickgust@mit.edu>
2022-01-25 20:07:05 -0500

    styling walletCard

--- client/src/components/modules/Wallets/WalletCard.js
+++ client/src/components/modules/Wallets/WalletCard.js
@@ -6,10 +6,10 @@ const HoldingCard = (props) => {
   let imgFile = "../icons/" + props.tokenObj.slug + ".png";
   return (
     <div className={"holdingCard"}>
-   <img src={imgFile} className={"coinHolding-img"}/>
+      <img src={imgFile} className={"coinHolding-img"}/>
       <div className={"holdingText"}>
-    <div className={"holdingAmount"}>{props.tokenObj.balance} {props.tokenObj.token}</div>
-      <div className={"holdingPerc"}>{(((props.tokenObj.balance*props.priceData[props.tokenObj.token]) / props.totalWalletVal) * 100).toFixed(2)}% of wallet value</div>
+        <div className={"holdingAmount"}>{props.tokenObj.balance} {props.tokenObj.token}</div>
+        <div className={"holdingPerc"}>{(((props.tokenObj.balance*props.priceData[props.tokenObj.token]) / props.totalWalletVal) * 100).toFixed(2)}% of wallet value</div>
       </div>
     </div>
   )
@@ -54,21 +54,22 @@ const [expanded, setExpanded] = useState(false)
   <div id="walletCard" className={"walletCard-container"}>
     <img src={imgFile} alt="icon" className="wallet-img" />
     <div className={"walletType"}>
-    <div className="walletCard-type">{props.name}</div>
+      <div className="walletCard-type">{props.name}</div>
       <div className={"totalValue"}>Total: ${totalWalletVal.toFixed(2)}</div>
       <div className={"totalPerc"}>{((totalWalletVal / props.totalVal) * 100).toFixed(2)}% of Portfolio Value</div>
     </div>
     <div id="holdings" className="walletCard-holdings hide">
-      Holdings:
+      <div className="holdings-title">Holdings:</div>
       <div className="walletCard-balance">{coinsList}</div>
     </div>
     <div id="transactions" className="walletCard-transactions hide">
-      Transaction History:
-      <TransactionFeed allTransactions={props.allTransactions}/>
+      <div className="transactions-title">Transaction History:</div>
+      <div className="transactions-feed"><TransactionFeed allTransactions={props.allTransactions} /></div>
     </div>
     <button id="dropdown" onClick={clickToExpandCards} className={"expand"}>▼</button>
   </div>
   );
 };
+
 
 export default WalletCard;
