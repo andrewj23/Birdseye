ff07268bef2b1d2a66acd2fe373c5d96983cc58b
bradydarby <bdarby@mit.edu>
2022-01-26 01:05:07 -0500

    moved nomics to client

--- client/nomics.js
+++ client/nomics.js
@@ -1,16 +1,17 @@
 import Nomics from "nomics";
 
-async function getCoinData(coins){
-  const nomics = new Nomics({
-    apiKey: 'm_be3c899ce66f1684d94cb70100d9d58bdce71222'
-  });
-  const currencies = await nomics.currenciesTicker({ids:[coins]});
-  let out = {};
-  out[coins]=parseFloat(currencies[0]['price'])
-  return out;
-};
+// async function getCoinData(coins){
+//   const nomics = new Nomics({
+//     apiKey: 'm_be3c899ce66f1684d94cb70100d9d58bdce71222'
+//   });
+//   const currencies = await nomics.currenciesTicker({ids:[coins]});
+//   let out = {};
+//   out[coins]=parseFloat(currencies[0]['price'])
+//   return out;
+// };
 
 async function getAllPrices(){
+  // return await get('api/getAllPrices')
   const nomics = new Nomics({
     apiKey: 'm_be3c899ce66f1684d94cb70100d9d58bdce71222'
   });
@@ -27,4 +28,4 @@ async function getAllPrices(){
 
 
 
-export {getCoinData, getAllPrices};
+export {getAllPrices};
